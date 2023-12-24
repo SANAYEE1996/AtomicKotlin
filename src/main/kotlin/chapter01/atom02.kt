e5a38b9f6648d5511ee70d0099fcc4f9e95f0542
SANAYEE1996 <dudtkd0219@gmail.com>
2023-12-24 15:10:54 +0900

    atom 02

---
+++ src/main/kotlin/chapter01/atom02.kt
@@ -0,0 +1,18 @@
+package chapter01
+
+fun main(){
+    var number = 11
+    var rate = 1.4
+    var words = "Park Young Sang"
+
+    println(number)
+    println(rate)
+    println(words)
+
+    var sum = 1
+    sum += 2
+    println(sum)
+
+    val mouse = "Logitech"
+    // mouse = 15 --> compile error
+}
