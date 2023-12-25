c73e7a29cbb157379d08cffd64e052662ed592be
SANAYEE1996 <dudtkd0219@gmail.com>
2023-12-25 14:46:33 +0900

    데이터 타입

---
+++ src/main/kotlin/chapter01/atom03.kt
@@ -0,0 +1,21 @@
+package chapter01
+
+fun main(){
+    println("My Account :: " +14.37)
+    // println("My Account :: " *14.37) --> error
+
+    val count: Int = 100
+    val param: Double = 4.7
+    val isMoney: Boolean = count > 0
+    val myName: String = "Park"
+    val code: Char = 'A'
+
+    println("count : $count")
+    println("param : $param")
+    println("isMoney : $isMoney")
+    println("myName : $myName")
+    println("code : $code")
+
+    val n = 1 + 1.3482
+    println(n)
+}
